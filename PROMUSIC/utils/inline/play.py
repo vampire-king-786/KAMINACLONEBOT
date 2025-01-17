ec27a3b9f9b3ed6ed168f4236372fec014702405
Rocky9852 <abhisheklkr628@gmail.com>
2025-01-17 22:13:37 +0530

    Update play.py

--- PROMUSIC/utils/inline/play.py
+++ PROMUSIC/utils/inline/play.py
@@ -229,10 +229,10 @@ def stream_markup2(_, chat_id):
         ],
 [
             InlineKeyboardButton(
-                text="ᴏᴡɴᴇʀ", url=f"https://t.me/ZeoXpro"
+                text="ᴏᴡɴᴇʀ", url=f"https://t.me/Rocky_dtm"
             ),
             InlineKeyboardButton(
-                text="ᴄʜᴀᴛ", url=f"https://t.me/ProBotGc"
+                text="ᴄʜᴀᴛ", url=f"https://t.me/SaregamaSupport"
             ),
         ],
 
