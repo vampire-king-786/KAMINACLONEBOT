64a498597c60bcafcc43ca554751dfcfc03d81e7
MOONl <mypersonalheruko@gmail.com>
2025-01-20 23:30:12 +0530

    Update help.py

--- PROMUSIC/cplugin/help.py
+++ PROMUSIC/cplugin/help.py
@@ -106,7 +106,7 @@ async def helper_cb(client, CallbackQuery, _):
     if cb == "hb9":
         if CallbackQuery.from_user.id not in SUDOERS:
             return await CallbackQuery.answer(
-                "😎 Pehle Zeo Ko Papa Bol Jake 😆😆", show_alert=True
+                "😎 Sᴏʀʀʏ ᴅᴏ ɴᴏᴛ ᴛʀʏ ᴇɴᴛᴇʀ ʜᴇʀ 😆😆", show_alert=True
             )
         else:
             await CallbackQuery.edit_message_text(helpers.HELP_9, reply_markup=keyboard)
