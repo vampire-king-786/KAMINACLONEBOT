0fe80cc0a237bde0860539ca7cf35c2a9b041206
MOONl <mypersonalheruko@gmail.com>
2025-01-20 23:17:45 +0530

    Update userbot.py

--- PROMUSIC/core/userbot.py
+++ PROMUSIC/core/userbot.py
@@ -23,8 +23,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("ProBotGc")
-                await self.one.join_chat("ProBotts")
+                await self.one.join_chat("untold_coder")
+                await self.one.join_chat("untold_coder")
             except:
                 pass
             assistants.append(1)
