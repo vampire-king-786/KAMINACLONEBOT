699cac859432b81e61a1a2d58ff121083e423bbf
MOONl <mypersonalheruko@gmail.com>
2025-01-20 21:19:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -54,7 +54,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Rocky9852/ZeixBots",
+    "https://github.com/MY-MOONREPO/ROCKY-",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
