d081a993014643d83b64025b7f9b6f929e225530
MOONl <mypersonalheruko@gmail.com>
2025-01-20 23:16:08 +0530

    Update zowner.py

--- PROMUSIC/plugins/tools/zowner.py
+++ PROMUSIC/plugins/tools/zowner.py
@@ -11,7 +11,6 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from PROMUSIC.utils.database import get_assistant
 import asyncio
 from PROMUSIC.misc import SUDOERS
-#from PROMUSIC.mongo.afkdb import LOGGERS as OWNERS
 from PROMUSIC.core.userbot import Userbot
 from pyrogram import Client, filters
 from pyrogram.errors import UserAlreadyParticipant
@@ -27,21 +26,19 @@ from pyrogram.errors import (
     UserNotParticipant,
 )
 from PROMUSIC import app
-#from PROMUSIC.utils.PROBin import admin_filter
-#from PROMUSIC.utils.decorators.userbotjoin import UserbotWrapper
 from PROMUSIC.utils.database import get_assistant, is_active_chat
 
 
 @app.on_message(filters.command("clone"))
 async def clones(client: Client, message: Message):
     await message.reply_photo(
-        photo=f"https://telegra.ph/file/1845472a641e97ac614a4.jpg",
+        photo=f"https://files.catbox.moe/oj3pfj.mp4",
         caption=f"""**🙂You Are Not Sudo User So You Are Not Allowed To Clone Me.**\n**😌Click Given Below Button And Host Manually Otherwise Contact Owner Or Sudo Users For Clone.**""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "🗡️ 𝐕1 𝐌ᴜsɪᴄ 𝐑ᴇᴘᴏ 🗡️", url=f"https://github.com/itzshukla/STRANGER-MUSIC2.0"
+                        "❮Oᴡɴᴇʀ❯", url=f"https://t.me/untold_coder4"
                     )
                 ]
             ]
