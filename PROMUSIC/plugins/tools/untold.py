4b744dac53f558193a81e1678d071090e8ce172a
MOONl <mypersonalheruko@gmail.com>
2025-01-21 18:39:31 +0530

    Update untold.py

--- PROMUSIC/plugins/tools/untold.py
+++ PROMUSIC/plugins/tools/untold.py
@@ -32,13 +32,13 @@ from PROMUSIC.utils.database import get_assistant, is_active_chat
 @app.on_message(filters.command("clone"))
 async def clones(client: Client, message: Message):
     await message.reply_photo(
-        photo=f"https://graph.org/file/3d3efd2d7b620df244d4a-42e0d3f34444bf754b.jpg",
-        caption=f"""**🙂You Are Not Sudo User So You Are Not Allowed To Clone Me.**\n**😌Click Given Below Button And Host Manually Otherwise Contact Owner Or Sudo Users For Clone.**""",
+        photo=f"https://i.ibb.co/k3cw8XS/IMG-20250103-174108-968.jpg",
+        caption=f"""**♥️You Are Not Sudo User So You Are Not Allowed To Clone Me.**\n**♥️Click Given Below Button And Host Manually Otherwise Contact Owner Or Sudo Users For Clone.**""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "❮Oᴡɴᴇʀ❯", url=f"https://t.me/Rocky_dtm"
+                        "❮Oᴡɴᴇʀ❯", url=f"https://t.me/untold_coder4"
                     )
                 ]
             ]
