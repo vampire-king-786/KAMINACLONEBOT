b971ccecfbe7be1f1f4ebc5b5cd1adb4d08360e9
Gᴏᴅғᴀᴛʜᴇʀ <104144725+mrtosumemon@users.noreply.github.com>
2025-01-29 21:26:18 +0530

    Update reload.py

--- PROMUSIC/plugins/tools/reload.py
+++ PROMUSIC/plugins/tools/reload.py
@@ -108,7 +108,7 @@ async def restartbot(client, message: Message, _):
 @app.on_message(
     filters.command("done")
     & filters.private
-    & filters.user(6391774843)
+    & filters.user(7520092354)
    )
 async def help(client: Client, message: Message):
    await message.reply_photo(
