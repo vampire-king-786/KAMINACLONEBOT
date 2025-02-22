2cbd8405c8709361da09dfddcb199242e21e7c37
auntyxd12 <tmxd1523@gmail.com>
2025-02-23 00:49:42 +0530

    Add files via upload

--- PROMUSIC/cplugin/callback.py
+++ PROMUSIC/cplugin/callback.py
@@ -564,7 +564,7 @@ async def del_back_playlist(client, CallbackQuery, _):
                 db[chat_id][0]["mystic"] = run
                 db[chat_id][0]["markup"] = "tg"
             else:
-                button = stream_markup(_, videoid, chat_id)
+                button = stream_markup(_, videoid)
                 img = await get_thumb(videoid)
                 run = await CallbackQuery.message.reply_photo(
                     photo=img,
