123a438811c32947e530974c2ea7f1cc5d85ed0b
auntyxd12 <tmxd1523@gmail.com>
2025-03-02 13:52:54 +0530

    Update clone.py

--- PROMUSIC/plugins/tools/clone.py
+++ PROMUSIC/plugins/tools/clone.py
@@ -36,7 +36,11 @@ C_BOT_COMMANDS = [
                 {"command": "/end", "description": "ᴄʟᴇᴀʀs ᴛʜᴇ ǫᴜᴇᴜᴇ ᴀɴᴅ ᴇɴᴅ ᴛʜᴇ ᴄᴜʀʀᴇɴᴛ ᴩʟᴀʏɪɴɢ sᴛʀᴇᴀᴍ."},
                 {"command": "/ping", "description": "ᴛʜᴇ ᴩɪɴɢ ᴀɴᴅ sʏsᴛᴇᴍ sᴛᴀᴛs ᴏғ ᴛʜᴇ ʙᴏᴛ."},
                 {"command": "/id", "description": "ɢᴇᴛ ᴛʜᴇ ᴄᴜʀʀᴇɴᴛ ɢʀᴏᴜᴘ ɪᴅ. ɪғ ᴜsᴇᴅ ʙʏ ʀᴇᴘʟʏɪɴɢ ᴛᴏ ᴀ ᴍᴇssᴀɢᴇ, ɢᴇᴛs ᴛʜᴀᴛ ᴜsᴇʀ's ɪᴅ."}
-
+                {"command": "/tagall", "description": "ᴛᴀɢ ᴀʟʟ ᴍᴇᴍʙᴇʀ ᴛᴏ ᴀᴄᴛɪᴠᴇ ɢʀᴏᴜᴘ."}
+                {"command": "/stoptagall", "description": "ᴛᴀɢ ᴀʟʟ ꜱᴛᴏᴘ."}
+                {"command": "/vctag", "description": "ᴠᴄ ᴛᴀɢ ᴛᴏ ɪɴᴠɪᴛᴇ ᴛᴏ ᴠᴏɪᴄᴇ ᴄʜᴀᴛ."}
+                {"command": "/stopvctag", "description": "ꜱᴛᴏᴘ ᴠᴄ ᴛᴀɢ ."}
+                 
             ]
 
 
