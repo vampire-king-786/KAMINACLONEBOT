c213430252b20a2259838a232e378af5f457b1d1
auntyxd12 <tmxd1523@gmail.com>
2025-03-17 01:21:10 +0530

    Update Stats.py

--- PROMUSIC/cplugin/Stats.py
+++ PROMUSIC/cplugin/Stats.py
@@ -9,17 +9,17 @@ from pyrogram.types import InputMediaPhoto, Message
 from pytgcalls.__version__ import __version__ as pytgver
 
 import config
-from ISTKHARMUSIC import app
-from ISTKHARMUSIC.core.userbot import assistants
-from ISTKHARMUSIC.misc import SUDOERS, mongodb
-from ISTKHARMUSIC.plugins import ALL_MODULES
-from ISTKHARMUSIC.utils.database import (
+from PROMUSIC import app
+from PROMUSIC.core.userbot import assistants
+from PROMUSIC.misc import SUDOERS, mongodb
+from PROMUSIC.plugins import ALL_MODULES
+from PROMUSIC.utils.database import (
     get_served_chats_clone,
     get_served_users_clone,
     get_sudoers,
 )
-from ISTKHARMUSIC.utils.decorators.language import language, languageCB
-from ISTKHARMUSIC.utils.inline.stats import back_stats_buttons, stats_buttons
+from PROMUSIC.utils.decorators.language import language, languageCB
+from PROMUSIC.utils.inline.stats import back_stats_buttons, stats_buttons
 from config import BANNED_USERS
 
 
